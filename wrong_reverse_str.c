4e9395abfcb41a3ab1e2cb3844f799dfd30bf3a8
yingfan2005 <yingfanzhang2005@gmail.com>
2025-03-29 12:39:44 -0300

    Create wrong_reverse_str.c

---
+++ wrong_reverse_str.c
@@ -0,0 +1,43 @@
+include<stdio.h>
+int my_stringlen(char* str)
+{
+    int count=0;
+    while(*str != '\0')
+    {
+        count ++;
+        str ++;
+    }
+}
+void reverse_str(char str[])
+{
+    int len = my_stringlen(str);
+    char tmp = str[0];
+    str[0]=str[len-1];
+    str[len-1]='\0';
+    if(my_stringlen(str+1)>= 2)
+    {
+        reverse_str(str+1);
+    }
+    str[len-1]= tmp;
+
+}
+/*void reverse_str(char arr[])
+{
+    int left = 0;
+    int right = my_stringlen(arr)-1;
+    while(left<right)
+    {
+        int tmp = arr[left];
+        arr[left]= arr[right];
+        arr[right]=tmp;
+        left ++;
+        right --;
+    }
+}*/
+int main ()
+{
+    char arr[]="abcdefg";
+    reverse_str(arr);
+    printf("%s\n",arr);
+    return 0;
+}
